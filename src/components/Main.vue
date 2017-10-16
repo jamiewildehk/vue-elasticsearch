3d314958475090060859bd760e48c33980c4d0bb
Haohong Xu <weidai1122@gmail.com>
2017-10-16 20:19:43 +0800

    Add Main route and component

---
+++ src/components/Main.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="main">
+
+    <img src="../assets/it2g.png" alt="miro" class="miroImg">
+    <h1>Add Visual Search to Your App or Project in <u>Minutes</u></h1>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  data () {
+    return {
+      msg: 'Main View'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+import Main from '@/components/Main'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'Hello',
+    //   component: HelloWorld
+    // },
     {
       path: '/',
-      name: 'Hello',
-      component: HelloWorld
+      name: 'Main',
+      component: Main
     }
   ]
 })
