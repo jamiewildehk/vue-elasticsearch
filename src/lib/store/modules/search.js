02b43fd9384f2006440fc511ccc01e92fa001d89
Haohong Xu <weidai1122@gmail.com>
2017-10-17 02:10:05 +0800

    Add getters and mutations

--- src/lib/store/modules/search.js
+++ src/lib/store/modules/search.js
@@ -1,21 +1,49 @@
+import * as types from '../mutation-types'
+
 // initial state
 const state = {
   keyword: '',
   fetching: false,
   total: 0,
-  hits: []
+  hits: [],
+  error: null
 }
 
 // getters
-const getters = {}
+const getters = {
+  keyword: state => state.keyword,
+  fetching: state => state.fetching,
+  total: state => state.total,
+  hits: state => state.hits,
+  error: state => state.error
+}
 
 // mutations
-const mutations = {}
+const mutations = {
+  [types.UPDATE_KEYWORD] (state, { keyword }) {
+    state.keyword = keyword
+  },
+
+  [types.SEARCH_REQUEST] (state) {
+    state.fetching = true
+  },
+
+  [types.SEARCH_SUCCESS] (state, { response }) {
+    // TODO: Parse response
+    state.fetching = false
+  },
+
+  [types.SEARCH_FAILURE] (state, { error }) {
+    state.fetching = false
+    state.error = error
+  }
+}
 
 // actions
 const actions = {}
 
 export default {
+  namespaced: true,
   state,
   getters,
   mutations,
--- src/lib/store/modules/suggest.js
+++ src/lib/store/modules/suggest.js
@@ -1,21 +1,49 @@
+import * as types from '../mutation-types'
+
 // initial state
 const state = {
   query: 'ok',
   fetching: false,
   total: 0,
-  keywords: []
+  keywords: [],
+  error: null
 }
 
 // getters
-const getters = {}
+const getters = {
+  query: state => state.query,
+  fetching: state => state.fetching,
+  total: state => state.total,
+  keywords: state => state.keywords,
+  error: state => state.error
+}
 
 // mutations
-const mutations = {}
+const mutations = {
+  [types.UPDATE_QUERY] (state, { query }) {
+    state.query = query
+  },
+
+  [types.SUGGEST_REQUEST] (state) {
+    state.fetching = true
+  },
+
+  [types.SUGGEST_SUCCESS] (state, { response }) {
+    // TODO: Parse response
+    state.fetching = false
+  },
+
+  [types.SUGGEST_FAILURE] (state, { error }) {
+    state.fetching = false
+    state.error = error
+  }
+}
 
 // actions
 const actions = {}
 
 export default {
+  namespaced: true,
   state,
   getters,
   mutations,
