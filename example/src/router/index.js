a6b7e6c9ea5d4f7f23f5d579fa742ebe9a96d1c9
Haohong Xu <weidai1122@gmail.com>
2017-10-26 06:51:19 -0700

    Remove HelloWorld component in example

--- example/src/router/index.js
+++ example/src/router/index.js
@@ -7,11 +7,6 @@ Vue.use(Router)
 
 export default new Router({
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'Hello',
-    //   component: HelloWorld
-    // },
     {
       path: '/',
       name: 'Example',
