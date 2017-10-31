7ea1415791f093feb86a209bb3e8662fcdc34cf1
Haohong Xu <weidai1122@gmail.com>
2017-10-30 20:49:10 -0700

    Add Result view component to example repo

---
+++ example/src/components/Result.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="result">
+
+    <h1>Result for {{ $route.params.keyword }}</h1>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Result',
+  data () {
+    return {
+      msg: 'Result View'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- example/src/router/index.js
+++ example/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import Example from '@/components/Example'
+import Result from '@/components/Result'
 
 Vue.use(Router)
 
@@ -11,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'Example',
       component: Example
+    },
+    {
+      path: '/result/:keyword',
+      name: 'Result',
+      component: Result
     }
   ]
 })
